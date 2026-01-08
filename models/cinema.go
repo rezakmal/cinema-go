1e7d63ca40a6d662c0e6857c9cb114fff43217d0
Reza Akmal <rezajakmal@gmail.com>
2026-01-08 21:13:30 +0700

    feat: add cinema domain model

---
+++ models/cinema.go
@@ -0,0 +1,18 @@
+package models
+
+import "time"
+
+type Cinema struct {
+	ID        int       `json:"id"`
+	Name      string    `json:"name"`
+	Location  string    `json:"location"`
+	Rating    float64   `json:"rating"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+type CreateCinemaRequest struct {
+	Name     string  `json:"name" binding:"required"`
+	Location string  `json:"location" binding:"required"`
+	Rating   float64 `json:"rating"`
+}
