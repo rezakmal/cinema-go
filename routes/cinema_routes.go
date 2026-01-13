311393df51b428dfcbaa5501ebd3e339f1bf638e
Reza Akmal <rezajakmal@gmail.com>
2026-01-13 22:58:08 +0700

    feat(routes): add cinema routes setup

---
+++ routes/cinema_routes.go
@@ -0,0 +1,18 @@
+package routes
+
+import (
+	"cinema-go/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupCinemaRoutes(router *gin.Engine, cinemaController *controllers.CinemaController) {
+	// Cinema routes
+	cinema := router.Group("/cinema")
+	{
+		cinema.POST("", cinemaController.CreateCinema)     // POST /cinema
+		cinema.GET("", cinemaController.GetCinemas)        // GET /cinema
+		cinema.GET("/:id", cinemaController.GetCinemaByID) // GET /cinema/:id
+		cinema.PUT("/:id", cinemaController.UpdateCinema)  // PUT /cinema/:id
+	}
+}
